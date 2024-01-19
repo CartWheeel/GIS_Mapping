cc2440b78a0ff11f87f80807696f89436b7bbe57
CartWheeel <carterhwms@gmail.com>
2024-01-19 11:29:35 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+require(["esri/Map", "esri/views/MapView"], (Map, MapView) => {
+  const map = new Map({
+    basemap: "topo"
+  });
+
+  const view = new MapView({
+    container: "viewDiv",
+    map: map,
+    zoom: 15,
+    center: [-111.7900, 43.8200] // longitude, latitude
+  });
+});
